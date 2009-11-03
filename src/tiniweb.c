a874d95d9187e60f11b199d4ab63e9541ccd08c6
Michael Gebetsroither <mgebetsroither@iaik.tugraz.at>
2009-11-03 13:32:06 +0100

    provide example response in tiniweb.c

--- src/tiniweb.c
+++ src/tiniweb.c
@@ -4,6 +4,7 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 /** tiniweb main routine
  * \param argc number of commandline arguments
@@ -14,5 +15,18 @@
  */
 int main(int argc, char** argv)
 {
+    // just to make tcp wrapper happy
+    char buf[8192];
+    int ret = read(STDIN_FILENO, buf, 8192);
+    fprintf(stderr, "tiniweb: got %d bytes\n", ret);
+
+    // sample response
+    printf("HTTP/1.1 200 OK\r\n"
+           "Server: tiniweb/1.0\r\n"
+           "Connection: close\r\n"
+           "Content-type: text/html\r\n"
+           "\r\n"
+           "<html><body>Hello!</body></html>\r\n");
+
     return EXIT_SUCCESS;
 }
