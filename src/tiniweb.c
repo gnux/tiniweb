68bcce158e8f4c6bc22a41c6c14065a9aff626a6
Patrick Plaschzug <patrick.plaschzug@student.tugraz.at>
2009-11-18 16:53:45 +0100

    merge again and again

--- src/tiniweb.c
+++ src/tiniweb.c
@@ -17,6 +17,8 @@
 #include "debug.h"
 #include "parser.h"
 #include "normalize.h"
+#include "envvar.h"
+
 
 // default values for options, if no command line option is available
 static const int SCI_CGI_TIMEOUT = 1;
@@ -100,14 +102,19 @@ int main(int argc, char** argv) {
       si_cgi_timeout_ = SCI_CGI_TIMEOUT;
     }
     
-
+    initEnvVarList("GATEWAY_INTERFACE","CGI/1.1");
+    //appendToEnvVarList("SCRIPT_FILENAME",scp_cgi_dir_);
+	//appendToEnvVarList("DOCUMENT_ROOT",scp_web_dir_);
 
     debug(0, "Argument parsing finished\n");
     debugVerbose(0, "WEB_DIR = %s \n", scp_web_dir_);
     debugVerbose(0, "CGI_DIR = %s \n", scp_cgi_dir_);
     debugVerbose(0, "CGI_TIMEOUT = %d \n", si_cgi_timeout_);
     
-    normalizeHttp(stdin);
+	http_norm *hnp_info = normalizeHttp(stdin);
+	debugVerbose(0, "Normalize finished \n");
+	parse(hnp_info);
+	debugVerbose(0, "Parsing finished \n"); 
 
     // I am testing!
     // just to make tcp wrapper happy 
