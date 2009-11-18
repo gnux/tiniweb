56662f3857d38ce26da34a2a3c20a4fa67b2a309
Patrick Plaschzug <patrick.plaschzug@student.tugraz.at>
2009-11-18 16:54:51 +0100

    first line parsing finished, starting argument parsing

--- src/parser.h
+++ src/parser.h
@@ -8,7 +8,7 @@
 #include "typedef.h"
 
 
-void parse(char input[], int max_bufsize);
+void parse(http_norm *hnp_info);
 void normalizeHeader(char input[]);
 bool isChar(char input);
 bool isWhiteSpace(char input);
@@ -17,6 +17,7 @@ bool isNewLine(char input);
 int parseHttpRequestHeader(char* input, char** outputline, int offset);
 int parseRequestLine(char* input, char** outputline, int offset);
 int parseMethod(char* input, char** outputline, int offset);
+int parseArguments(http_norm *hnp_info, char** outputline);
 int parseHttpVersion(char* input, char** outputline, int offset);
 int parseHttpRequest(char* input, char** outputline, int offset);
 int parseRequestURI(char* input, char** outputline, int offset);
