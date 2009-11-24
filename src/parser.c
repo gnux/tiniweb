6d646f636c46ba9b9d591db878b53584b41bad0c
Georg Neubauer <georg.neubauer@student.tugraz.at>
2009-11-24 12:26:23 +0100

    worked on parser

--- src/parser.c
+++ src/parser.c
@@ -21,6 +21,7 @@ typedef struct body_info {
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <math.h>
 #include "typedef.h"
 #include "parser.h"
 #include "secmem.h"
@@ -28,131 +29,193 @@ typedef struct body_info {
 #include "debug.h"
 #include "envvar.h"
 
+static const enum SCE_KNOWN_METHODS {
+  GET = 0,
+  POST,
+  HEAD,
+  UNKNOWN
+} SCE_METHOD;
 
-static const char BLANKS[] = " \t";
-static const char BLANKS_NEW_LINE[] = " \t\n";
-static const char* METHOD[] = {"GET", "POST", "HEAD"};
-static const char HTTPVERSION[] = "HTTP/1.1";
+static const char* SCCA_KNOWN_METHODS[] = {"GET", "POST", "HEAD"};
+static const int SCI_NUM_KNOWN_METHODS = 3;
+static const char* SCCP_KNOWN_HTTPVERSION = "HTTP/1.1";
+static const char* SCCP_HTTP_HEADER_FIELD_MARKER = "HTTP_";
+static enum SCE_KNOWN_METHODS se_used_method = UNKNOWN;
 
-int MAX_HEADER_BUFSIZE=0;
-bool selectedMETHOD[] = {FALSE, FALSE, FALSE};
-bool foundARGUMENTS[] = {FALSE};
-/*
- * - char* first line
-- char* normalisierte http_req/rep
-- char* body
-- char** header_fields
-- char** header_bodys
-- size_t anzahl_fields
- */
-
-void parse(http_norm *hnp_info){
-	
-	
-	char** outputline = NULL;	
-	MAX_HEADER_BUFSIZE = strlen(hnp_info->cp_header);
-	int offset = 0;
-	int arguments_valid = 0;
-	int first_line_valid =  parseHttpRequestHeader(hnp_info->cp_first_line, outputline, offset);
-	if(first_line_valid == EXIT_FAILURE){
-		//TODO: ERROR
-	}
-	
-	if(selectedMETHOD[0]==TRUE){
-		appendToEnvVarList("REQUEST_METHOD","GET");
-		/*if(hnp_info->cpp_header_field_body != NULL){
-			arguments_valid=EXIT_FAILURE;
-			//TODO: ERROR
-		}
-		else*/
-			arguments_valid = parseArguments(hnp_info,outputline);
-	}
-	else if(selectedMETHOD[1]==TRUE){
-		appendToEnvVarList("REQUEST_METHOD","POST");
-		if(hnp_info->cpp_header_field_body == NULL){
-			arguments_valid=EXIT_FAILURE;
-			//TODO: ERROR
-		}
-		else
-			arguments_valid = parseArguments(hnp_info,outputline);
-	}
-	else if(selectedMETHOD[2]==TRUE){
-		appendToEnvVarList("REQUEST_METHOD","HEAD");
-		if(hnp_info->cpp_header_field_body != NULL){
-			arguments_valid=EXIT_FAILURE;
-			//TODO: ERROR
-		}
-		else
-			arguments_valid = parseArguments(hnp_info,outputline);
-	}
-	else{
-		//can't happen
-	}
-	
-	if(arguments_valid==EXIT_FAILURE){
-		//TODO: ERROR
-	}
-	debugVerbose(3, "Arguments valid\n");
-
+int min(int a, int b){
+  return a < b ? a : b;
 }
 
-int parseArguments(http_norm *hnp_info, char** outputline){
-	debugVerbose(3, "Parse Arguments\n");
-	
-	
+void parse(http_norm *hnp_info){
+	//TODO: ERROR HANDLING
+	if(parseHttpRequestHeader(hnp_info->cp_first_line) == EXIT_FAILURE)
+	  secAbort();
+	if(parseArguments(hnp_info) == EXIT_FAILURE)
+	  secAbort();
+}
+
+int parseArguments(http_norm *hnp_info){
 	for(size_t i = 0; i < hnp_info->i_num_fields; ++i){
-		char cp_name_to_add[] = "HTTP_";
 		char* cp_name = NULL;
-		strAppend(&cp_name, cp_name_to_add);
+		strAppend(&cp_name, SCCP_HTTP_HEADER_FIELD_MARKER);
 		strAppend(&cp_name, hnp_info->cpp_header_field_name[i]);
 		stringToUpperCase(cp_name);
 		appendToEnvVarList(cp_name,hnp_info->cpp_header_field_body[i]);
 	}
-
-
-	return TRUE;	
+	// set Constants
+	appendToEnvVarList("GATEWAY_INTERFACE", "CGI/1.1");
+	appendToEnvVarList("SERVER_SOFTWARE", "tiniweb/1.0");
+	//TODO: What does it mean???? Which Content, body???
+	appendToEnvVarList("CONTENT_LENGHT", "0");
+	//TODO:Where to get???
+	appendToEnvVarList("REMOTE_USER","0");
+	//TODO:Where to get???
+	appendToEnvVarList("SCRIPT_FILENAME","0");
+	//TODO:Where to get???
+	appendToEnvVarList("DOCUMENT_ROOT","0");
+	return EXIT_SUCCESS;	
 }
 
-int parseHttpRequestHeader(char* input, char** outputline, int offset){
-	
-	debugVerbose(3, "Parse HttpRequestHeader\n");
-	offset = parseRequestLine(input,outputline,offset);
-	if(offset!=EXIT_FAILURE){
-		debugVerbose(3, "Method found\n");
-		return TRUE;
+int parseHttpRequestHeader(char* input){
+	debugVerbose(PARSER, "Parse HttpRequestHeader\n");
+	return parseRequestLine(input);
+}
+
+int parseRequestLine(char* input){
+	int i_offset = parseMethod(input, 0);
+	switch (se_used_method){
+	  case POST: // TODO: provide BODY Field in this case!
+	  case GET:
+	  case HEAD:
+	    appendToEnvVarList("REQUEST_METHOD",SCCA_KNOWN_METHODS[se_used_method]);
+	    break;
+	  default:
+	    debugVerbose(PARSER, "Unknown request Method detected, next step is to abort\n");
+	    return EXIT_FAILURE;
+	    break;
+	};
+	i_offset = parseRequestURI(input, i_offset);
+	if( i_offset == EXIT_FAILURE){
+	  debugVerbose(PARSER, "Failure in request line detected, next step is to abort\n");
+	  return EXIT_FAILURE;
 	}
-	return EXIT_FAILURE;	
+	if(parseHttpVersion(input, i_offset + 1) == EXIT_FAILURE){
+	  debugVerbose(PARSER, "Unknown server protocol, next step is to abort\n");
+	  return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
 }
 
-int parseRequestLine(char* input, char** outputline, int offset){
-	
-	int new_offset=0;
-	new_offset = parseMethod(input,outputline,offset);
-	parseRequestURI(input, NULL, 0);
-	/*if(new_offset!=EXIT_FAILURE){
-		if(input[new_offset]== ' '){
-			new_offset = offsetPP(new_offset, 1);
-			new_offset = parseRequestURI(input,outputline,new_offset);
-			if(new_offset!=EXIT_FAILURE && isEOF(input[new_offset])==FALSE){
-				if(isWhiteSpace(input[new_offset])==TRUE){
-					new_offset = offsetPP(new_offset, 1);
-					new_offset = parseHttpVersion(input,outputline,new_offset);
-					if(new_offset!=EXIT_FAILURE){
-						if(isEOF(input[new_offset])==TRUE){
-							new_offset = offsetPP(new_offset, 1);
-							return new_offset;
-						}
-					}
-				}
-			}
-		}
-	}*/
-	return EXIT_FAILURE;
-	
+int parseMethod(char* input, int offset){
+  int i_offset_en = 0;
+  int i = offset;
+  char* cp_method = NULL;
+  for(;i_offset_en < strlen(input) && input[i_offset_en] != ' '; ++i_offset_en);
+  cp_method = secGetStringPart(input, 0, i_offset_en - 1);
+  for(;i < SCI_NUM_KNOWN_METHODS; ++i)
+    if(strncmp(SCCA_KNOWN_METHODS[i], cp_method, min(strlen(SCCA_KNOWN_METHODS[i]), strlen(cp_method))) == 0){
+      se_used_method = i;
+      return i_offset_en + 1;
+    }
+  return EXIT_FAILURE;
 }
 
-int parseMethod(char* input, char** outputline, int offset){
-	bool was_right = TRUE;
+int parseRequestURI(char* input, int offset){
+	char* cp_uri = NULL;
+	char* cp_path = NULL;
+	char* cp_fragment = NULL;
+	char* cp_query = NULL;
+	int i_offset_st = 0;
+	int i_offset_en = 0;
+	for(i_offset_st = offset; i_offset_st < strlen(input) && input[i_offset_st] != '/'; ++i_offset_st);
+	if(i_offset_st >= strlen(input)){
+	  debugVerbose(PARSER, "No URI found\n");
+	  return EXIT_FAILURE;
+	}
+	for(i_offset_en = i_offset_st; i_offset_en < strlen(input) && input[i_offset_en] != ' '; ++i_offset_en);
+	cp_uri = secGetStringPart(input, i_offset_st, i_offset_en - 1);
+	for(i_offset_st = 0; i_offset_st < strlen(cp_uri) && cp_uri[i_offset_st] != '?'; ++i_offset_st);
+	if(i_offset_st != strlen(cp_uri)){
+	  cp_path = secGetStringPart(cp_uri, 0, i_offset_st - 1);
+	  for(i_offset_en = i_offset_st; i_offset_en < strlen(cp_uri) && cp_uri[i_offset_en] != '#'; ++i_offset_en);
+	  cp_query = secGetStringPart(cp_uri, i_offset_st + 1, i_offset_en - 1);
+	  if(i_offset_en != strlen(cp_uri))
+	    cp_fragment = secGetStringPart(cp_uri, i_offset_en + 1, strlen(cp_uri) - 1);
+	}
+	else
+	  strAppend(&cp_path, cp_uri);
+	if(cp_uri)
+	  appendToEnvVarList("REQUEST_URI",cp_uri);
+	if(cp_path){
+	  if(validateString(cp_path) == EXIT_FAILURE){
+	    debugVerbose(PARSER, "Invalid char detected in URI Path\n");
+	    return EXIT_FAILURE;
+	  }
+	  appendToEnvVarList("REQUEST_PATH", cp_path);
+	}
+	if(cp_query)
+	    appendToEnvVarList("QUERY_STRING",cp_query);
+	if(cp_fragment)
+	  appendToEnvVarList("FRAGMENT",cp_fragment);
+	return offset + strlen(cp_uri);
+}
+
+int parseHttpVersion(char* input, int offset){
+  char* cp_http_version = NULL;
+  if(offset > strlen(input))
+    cp_http_version = secGetStringPart(SCCP_KNOWN_HTTPVERSION, 0, strlen(SCCP_KNOWN_HTTPVERSION));
+  else    
+    cp_http_version = secGetStringPart(input, offset, strlen(input));
+  if(strncmp(cp_http_version, SCCP_KNOWN_HTTPVERSION, min(strlen(SCCP_KNOWN_HTTPVERSION), strlen(cp_http_version))) != 0)
+    return EXIT_FAILURE;
+  appendToEnvVarList("SERVER_PROTOCOL",cp_http_version);
+}
+
+int validateString(char* cp_string){
+  int i = 0;
+  for(;i<strlen(cp_string); ++i)
+    if(isNonEscapedChar(cp_string, i) == TRUE)
+      return EXIT_FAILURE;
+  return EXIT_SUCCESS;
+}
+
+bool isNonEscapedChar(char* input, int i_offset){
+  const char* ccp_invalids = " ;?:@&=+$,#";
+  int i = 0;
+  for(; i<strlen(ccp_invalids); ++i)
+    if(input[i_offset] == ccp_invalids[i])
+      return TRUE;
+  if(input[i_offset]=='%'){
+    if(strlen(input) - i_offset < 3)
+      return TRUE;
+    else if(isHexDigit(input[i_offset + 1]) == FALSE || isHexDigit(input[i_offset + 2]) == FALSE)
+      return TRUE;
+    else;
+  }
+  return FALSE;
+}
+
+bool isHexDigit(char input){
+  if(input>0x29 && input < 0x3A)
+    return TRUE;
+  else if(input>0x40 && input < 0x47)
+    return TRUE;
+  else if(input>0x60 && input < 0x67)
+    return TRUE;
+  else
+    return FALSE;
+}
+
+void stringToUpperCase(char* input){
+	for(int i=0; i<strlen(input);i++){
+		input[i] = toupper(input[i]);
+		if(input[i] == '-')
+		  input[i] = '_';
+	}	
+}
+
+
+      /*bool was_right = TRUE;
 	if(offset == 0){
 		for(int i=0; i<=2; i++){
 			was_right = TRUE;
@@ -174,13 +237,9 @@ int parseMethod(char* input, char** outputline, int offset){
 	}
 	
 	return EXIT_FAILURE;	
-}
-
-int parseHttpVersion(char* input, char** outputline, int offset){
+}*/
 	
-	bool istrue = TRUE;
-	
-	for(int i=0; i<strlen(HTTPVERSION);i++){
+/*	for(int i=0; i<strlen(HTTPVERSION);i++){
 		if(HTTPVERSION[i]!=input[offset]){
 			istrue = FALSE;
 			break;
@@ -192,94 +251,64 @@ int parseHttpVersion(char* input, char** outputline, int offset){
 		return offset;
 	}
 	else
-		return EXIT_FAILURE;
-}
+		*/
+//return EXIT_FAILURE;
+//}
 
-int parseHttpRequest(char* input, char** outputline, int offset){
-	return offset;
-}
 
-int parseRequestURI(char* input, char** outputline, int offset){
+	//if(parseArguments());
+	//char** outputline = NULL;	
+	//MAX_HEADER_BUFSIZE = strlen(hnp_info->cp_header);
+	//int offset = 0;
+	//int arguments_valid = 0;
+	//TODO: remove args 2,3
 	
-//##################################################################################
-	char* cp_uri = NULL;
-	char* cp_path = NULL;
-	char* cp_fragment = NULL;
-	char* cp_query = NULL;
-	char* my_char = NULL;
-	char* cgi_path = NULL;
-	bool fragment_found = FALSE;
-	bool query_found = FALSE;
-	bool cgi_bin_found = FALSE;
-	int save_position = 0;
-	int i_offset_st = 0;
-	int i_offset_en = 0;
-	for(i_offset_st = 0; i_offset_st < strlen(input) && input[i_offset_st] != '/'; ++i_offset_st);
-	if(i_offset_st == strlen(input))
-	  secAbort();
-	for(i_offset_en = i_offset_st; i_offset_en < strlen(input) && input[i_offset_en] != ' '; ++i_offset_en);
-	cp_uri = secGetStringPart(input, i_offset_st, i_offset_en - 1);
-	for(i_offset_st = 0; i_offset_st < strlen(cp_uri) && cp_uri[i_offset_st] != '?'; ++i_offset_st);
-	if(i_offset_st != strlen(cp_uri)){
-	  cp_path = secGetStringPart(cp_uri, 0, i_offset_st - 1);
-	  for(i_offset_en = i_offset_st; i_offset_en < strlen(cp_uri) && cp_uri[i_offset_en] != '#'; ++i_offset_en);
-	  cp_query = secGetStringPart(cp_uri, i_offset_st + 1, i_offset_en - 1);
-	  if(i_offset_en != strlen(cp_uri)){
-	    cp_fragment = secGetStringPart(cp_uri, i_offset_en + 1, strlen(cp_uri) - 1);
-	  }
-	}
-	else{
-	  strAppend(&cp_path, cp_uri);
-	}
-	if(cp_uri)
-	  appendToEnvVarList("REQUEST_URI",cp_uri);
-	if(cp_path){
-	  if(validateString(cp_path) == EXIT_FAILURE)
-	    secAbort();
-	  appendToEnvVarList("REQUEST_PATH", cp_path);
-	}
-	if(cp_query){
-/*	  if(validateString(cp_query) == EXIT_FAILURE)
-	    secAbort();*/
-	    appendToEnvVarList("QUERY_STRING",cp_query);
-	}
-	if(cp_fragment){
-/*	  if(validateString(cp_fragment) == EXIT_FAILURE)
-	    secAbort();*/
-	  appendToEnvVarList("FRAGMENT",cp_fragment);  
-	}
-	return i_offset_en;
-/*	  
-	  
-	  
-	  
-	//	
-	  return i_offset_en;
+	//if(first_line_valid == EXIT_FAILURE){
+		//TODO: ERROR
+	//}
 	
-	
-	if()
-	  //TODO: SET ENV!
-	  
-	//cp_path = secGetStringPart(cp_uri, 0, i_offset_st - 1);
-	
-	fprintf(stderr, "gugu!!! \n \n");
-	
-	
-	cp_query = secGetStringPart(cp_uri, i_offset_st + 1, i_offset_en - 1);
-	//for(i_offset_st = 0; i_offset_st < strlen(cp_uri) && cp_uri[i_offset_st] != ' '; ++i_offset_st);
-	if(i_offset_en == strlen(cp_uri)){
-	  //TODO: SET ENV!
-	  fprintf(stderr, "baba!!! \n \n");
-	  return i_offset_en;}
-	
-	fprintf(stderr, "gugu!!! \n \n");
-	//for(i_offset_en = i_offset_st; i_offset_en < strlen(cp_uri) && cp_uri[i_offset_en] != ' '; ++i_offset_en);
-	cp_fragment = secGetStringPart(cp_uri, i_offset_en + 1, strlen(cp_uri) - 1);
-	
-	if(validateString(cp_path) == EXIT_FAILURE && validateString(cp_query) == EXIT_FAILURE && validateString(cp_fragment) == EXIT_FAILURE)
-	  secAbort();
-	
-	
+// 	if(selectedMETHOD[0]==TRUE){
+// 		appendToEnvVarList("REQUEST_METHOD","GET");
+// 		/*if(hnp_info->cpp_header_field_body != NULL){
+// 			arguments_valid=EXIT_FAILURE;
+// 			//TODO: ERROR
+// 		}
+// 		else*/
+// 			arguments_valid = parseArguments(hnp_info,outputline);
+// 	}
+// 	else if(selectedMETHOD[1]==TRUE){
+// 		appendToEnvVarList("REQUEST_METHOD","POST");
+// 		if(hnp_info->cpp_header_field_body == NULL){
+// 			arguments_valid=EXIT_FAILURE;
+// 			//TODO: ERROR
+// 		}
+// 		else
+// 			arguments_valid = parseArguments(hnp_info,outputline);
+// 	}
+// 	else if(selectedMETHOD[2]==TRUE){
+// 		appendToEnvVarList("REQUEST_METHOD","HEAD");
+// 		if(hnp_info->cpp_header_field_body != NULL){
+// 			arguments_valid=EXIT_FAILURE;
+// 			//TODO: ERROR
+// 		}
+// 		else
+// 			arguments_valid = parseArguments(hnp_info,outputline);
+// 	}
+// 	else{
+// 		//can't happen
+// 	}
+// 	
+// 	if(arguments_valid==EXIT_FAILURE){
+// 		//TODO: ERROR
+// 	}
+// 	debugVerbose(3, "Arguments valid\n");
+
+//int parseHttpRequest(char* input, char** outputline, int offset){
+//	return offset;
+//}
+
+
+
 // 	if(input[offset]=='/'){
 // 		//If it is an cgi request we have to build our path from the uri
 // 		/*int length = strlen(input)-1;
@@ -296,7 +325,7 @@ int parseRequestURI(char* input, char** outputline, int offset){
 // 			}
 // 			strncpy(cgi_path,input+offset+9,save_position);
 // 			debugVerbose(3, "CGI path %s\n",cgi_path);
-// 		}*/
+// 		}
 // 		
 // 		//Try to find a fragment and/or the request
 // 		while(isWhiteSpace(input[offset])==FALSE && isEOF(input[offset])==FALSE){
@@ -339,80 +368,45 @@ int parseRequestURI(char* input, char** outputline, int offset){
 //	else{
 //		return EXIT_FAILURE;
 //	}
-//	*/
-}
+//	
+//}
 
-bool isChar(char input){
-	if(input < 32 || input > 126 || input!='\t')
-		return FALSE;
-	else
-		return TRUE;
-}
-
-
-int validateString(char* cp_string){
-  int i = 0;
-  for(;i<strlen(cp_string); ++i)
-    if(isNonEscapedChar(cp_string, i) == TRUE)
-      return EXIT_FAILURE;
-  return EXIT_SUCCESS;
-}
-
-bool isNonEscapedChar(char* input, int i_offset){
-  const char* ccp_invalids = " ;?:@&=+$,#";
-  int i = 0;
-  for(; i<strlen(ccp_invalids); ++i)
-    if(input[i_offset] == ccp_invalids[i])
-      return TRUE;
-  if(input[i_offset]=='%')
-    if(strlen(input) - i_offset < 3)
-      return TRUE;
-    else if(isHexDigit(input[i_offset + 1]) == FALSE || isHexDigit(input[i_offset + 2]) == FALSE)
-      return TRUE;
-  return FALSE;
-}
-
-bool isWhiteSpace(char input){
-	if(input==' ' || input=='\t')
-			return TRUE;
-	return FALSE;
-}
-
-bool isNewLine(char input){
-	if(input=='\n')
-		return TRUE;
-	return FALSE;
-}
-
-bool isHexDigit(char input){
-  if(input>0x29 && input < 0x3A)
-    return TRUE;
-  else if(input>0x40 && input < 0x47)
-    return TRUE;
-  else if(input>0x60 && input < 0x67)
-    return TRUE;
-  else
-    return FALSE;
-}
+// bool isChar(char input){
+// 	if(input < 32 || input > 126 || input!='\t')
+// 		return FALSE;
+// 	else
+// 		return TRUE;
+// }
 
 
-bool isEOF(char input){
-	if(input=='\0')
-		return TRUE;
-	return FALSE;
-}
 
-int offsetPP(int offset, int count){
-	if(offset+count < MAX_HEADER_BUFSIZE){
-		offset= offset + count;
-	}
-	return offset;
-}
 
-void stringToUpperCase(char* input){
-	
-	for(int i=0; i<strlen(input);i++){
-		input[i] = toupper(input[i]);
-	}	
-}
+// /*bool isWhiteSpace(char input){
+// 	if(input==' ' || input=='\t')
+// 			return TRUE;
+// 	return FALSE;
+// }
+// 
+// bool isNewLine(char input){
+// 	if(input=='\n')
+// 		return TRUE;
+// 	return FALSE;
+// }
+// 
+// 
+// 
+// 
+// bool isEOF(char input){
+// 	if(input=='\0')
+// 		return TRUE;
+// 	return FALSE;
+// }
+// 
+// int offsetPP(int offset, int count){
+// /*	if(offset+count < MAX_HEADER_BUFSIZE){
+// 		offset= offset + count;
+// 	}
+// 	return offset;*/
+// }*/
+
 
--- src/parser.h
+++ src/parser.h
@@ -7,30 +7,69 @@
 #define PARSER_H_
 #include "typedef.h"
 
-
+/**
+ *
+ */
 void parse(http_norm *hnp_info);
-void normalizeHeader(char input[]);
-bool isChar(char input);
-bool isWhiteSpace(char input);
-bool isNewLine(char input);
-bool isEOF(char input);
-bool isNonEscapedChar(char* input, int i_offset);
-bool isHexDigit(char input);
 
-void stringToUpperCase(char* input);
+/**
+ *
+ */
+int parseArguments(http_norm *hnp_info);
 
-int parseHttpRequestHeader(char* input, char** outputline, int offset);
-int parseRequestLine(char* input, char** outputline, int offset);
-int parseMethod(char* input, char** outputline, int offset);
-int parseArguments(http_norm *hnp_info, char** outputline);
-int parseHttpVersion(char* input, char** outputline, int offset);
-int parseHttpRequest(char* input, char** outputline, int offset);
-int parseRequestURI(char* input, char** outputline, int offset);
+/**
+ *
+ */
+int parseHttpRequestHeader(char* input);
 
-int offsetPP(int offset, int count);
+/**
+ *
+ */
+int parseRequestLine(char* input);
 
+/**
+ *
+ */
+int parseMethod(char* input, int offset);
+
+/**
+ *
+ */
+int parseRequestURI(char* input, int offset);
+
+/**
+ *
+ */
+int parseHttpVersion(char* input, int offset);
+
+/**
+ *
+ */
 int validateString(char* cp_string);
 
+/**
+ *
+ */
+bool isNonEscapedChar(char* input, int i_offset);
 
+/**
+ *
+ */
+bool isHexDigit(char input);
+
+/**
+ *
+ */
+void stringToUpperCase(char* input);
+
+
+
+//void normalizeHeader(char input[]);
+//bool isChar(char input);
+//bool isWhiteSpace(char input);
+//bool isNewLine(char input);
+//bool isEOF(char input);
+//int parseHttpRequest(char* input, char** outputline, int offset);
+//int offsetPP(int offset, int count);
 
 #endif /*PARSER_H_*/
