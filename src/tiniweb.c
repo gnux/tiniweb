521c8b7c0e5adac1a3263ed2d8acb1b613f23268
Christian Partl <cpartl@student.tugraz.at>
2009-11-26 10:23:10 +0100

    little change in tiniweb

--- src/tiniweb.c
+++ src/tiniweb.c
@@ -221,7 +221,7 @@ int main(int argc, char** argv) {
     //           "<html><body>Hello!</body></html>\r\n");   
 
 
-    processCGIScript("testscript", "Testbody");
+    processCGIScript("testscript");
     
 //     sec_test();
 //     sec_test();
