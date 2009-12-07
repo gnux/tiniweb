88bcbec93f181f29b02c2496962a302b25ae5145
Christian Partl <cpartl@student.tugraz.at>
2009-12-07 14:48:08 +0100

    documentation

--- src/httpresponse.h
+++ src/httpresponse.h
@@ -32,22 +32,84 @@ static const enum SCE_CONTENT_TYPE_
     
 } SCE_CONTENT_TYPE;
 
-int writeToOutputStream(int i_fd, const char* ccp_text);
+/**
+ * Writes a string to the specified file using the cgi timeout as timeout for writing.
+ *
+ * @param i_fd File descripter that shall be written to.
+ * @param ccp_text String that shall be written to the file.
+ * @return EXIT_SUCCESS if no problem occurred, EXIT_FAILURE otherwise.
+ */
+int writeStringToFile(int i_fd, const char* ccp_text);
 
+/**
+ * Sends a standard http message for the specified status code.
+ *
+ * @param i_status Status code (from SCE_STATUS_CODE).
+ * @return EXIT_SUCCESS if no problem occurred, EXIT_FAILURE otherwise.
+ */
 int sendHTTPErrorMessage(int i_status);
 
+/**
+ * Sends the response header of a cgi response.
+ *
+ * @param http_cgi_response Structure that contains all necessary header information.
+ * @return EXIT_SUCCESS if no problem occurred, EXIT_FAILURE otherwise.
+ */
 int sendCGIHTTPResponseHeader(http_cgi_response *header);
 
+/**
+ * Sends the an authorization response to the http client
+ *
+ * @param ccp_realm Realm that shall be used for authentication.
+ * @param ccp_nonce Nonce that shall be used for authentication.
+ * @return EXIT_SUCCESS if no problem occurred, EXIT_FAILURE otherwise.
+ */
 int sendHTTPAuthorizationResponse(const char* ccp_realm, const char* ccp_nonce);
 
+/**
+ * Sends a http response header to the client.
+ *
+ * @param i_status Status code (from SCE_STATUS_CODE).
+ * @param i_content_type Content type (from SCE_CONTENT_TYPE).
+ * @param i_content_lenth Content length.
+ * @return EXIT_SUCCESS if no problem occurred, EXIT_FAILURE otherwise.
+ */
 int sendHTTPResponseHeader(int i_status, int i_content_type, int i_content_length);
 
+/**
+ * Sends a http response to the client.
+ *
+ * @param i_status Status code (from SCE_STATUS_CODE).
+ * @param i_content_type Content type (from SCE_CONTENT_TYPE).
+ * @param ccp_body Message body.
+ * @return EXIT_SUCCESS if no problem occurred, EXIT_FAILURE otherwise.
+ */
 int sendHTTPResponse(int i_status, int i_content_type, const char* ccp_body);
 
+/**
+ * Sends a http response header to the client.
+ *
+ * @param cp_status Status code.
+ * @param cp_content_type Content type.
+ * @param i_content_lenth Content length.
+ * @return EXIT_SUCCESS if no problem occurred, EXIT_FAILURE otherwise.
+ */
 int sendHTTPResponseHeaderExplicit(const char* cp_status, const char* cp_content_type, int i_content_length);
 
+/**
+ * Get the status code as string from its SCE_STATUS_CODE identifier.
+ *
+ * @param i_status Status code (from SCE_STATUS_CODE).
+ * @return Text of the status code.
+ */
 char* getStatusCode(int i_status);
 
+/**
+ * Get the content type as string from its SCE_STATUS_CODE identifier.
+ *
+ * @param i_status Status code (from SCE_STATUS_CODE).
+ * @return Content type as text.
+ */
 char* getContentType(int i_content_type);
 
 #endif
--- src/staticfile.h
+++ src/staticfile.h
@@ -6,9 +6,21 @@
 #ifndef __STATIC_FILE_H__
 #define __STATIC_FILE_H__
 
+/**
+ * Sends a HTTP response including the specified static file.
+ *
+ * @param cp_path Path (including filename) to the file
+ */
 void processStaticFile(const char* path);
 
-int writeFileTo(int i_fd, int i_dest_fd);
+/**
+ * Writes the file of a source file descriptor to the destination file descriptor.
+ *
+ * @param i_src_fd File descriptor of the source
+ * @param i_dest_fd File descriptor of the destination
+ * @return EXIT_SUCCESS if no problem occurred, EXIT_FAILURE otherwise.
+ */
+int writeFileTo(int i_src_fd, int i_dest_fd);
 
 #endif
 
