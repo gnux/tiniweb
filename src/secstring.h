7ec8e32866bcb8348cb874ed45d1feb007469956
Dieter Ladenhauf <dieter.ladenhauf@gmail.com>
2012-04-01 21:35:35 +0200

    Added gpl header to .h files

--- src/secstring.h
+++ src/secstring.h
@@ -1,7 +1,33 @@
-/** secstring.h
-* Definition of secString functions
-* \file typedef.h
-* \author Patrick Plaschzug, Christian Partl, Georg Neubauer, Dieter Ladenhauf
+/** 
+ * Copyright 2009-2012
+ * 
+ * Dieter Ladenhauf
+ * Georg Neubauer
+ * Christian Partl
+ * Patrick Plaschzug
+ * 
+ * This file is part of Wunderwuzzi.
+ * 
+ * Wunderwuzzi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Wunderwuzzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Wunderwuzzi. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * -------------------------------------------------------------------
+ * 
+ * secstring.h
+ * Definition of secString functions
+ * 
+ * \file secstring.h
+ * \author Patrick Plaschzug, Christian Partl, Georg Neubauer, Dieter Ladenhauf
 */
 
 #ifndef __SEC_STRING_H__
